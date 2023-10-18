cb802fb7b71743e9cfed56900761902c220f8208
Pedro-Wilker <pedrowilker1516@gmail.com>
2023-10-18 16:31:31 -0300

    Criando a aba de banner

---
+++ src/components/Banner.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap"
+import {ArrowRightCircle} from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg"
+
+export const Banner = () => {
+        const [loopNum, setLoopNum] = useState(0);
+        const [isDeleting, setIsDeleting] = useState(false);
+        const [text, setText] = useState('');
+        const [delta, setDelta] = useState(100 - Math.random() * 100);
+        const [index, setIndex] = useState(1);
+        const toRotate = [ "Dev Front-End", "Dev Back-End", "Dev FullStack" ];
+        const period = 2000;
+      
+        useEffect(() => {
+          let ticker = setInterval(() => {
+            tick();
+          }, delta);
+      
+          return () => { clearInterval(ticker) };
+        }, [text])
+      
+        const tick = () => {
+          let i = loopNum % toRotate.length;
+          let fullText = toRotate[i];
+          let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+      
+          setText(updatedText);
+      
+          if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+          }
+      
+          if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+          } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+          } else {
+            setIndex(prevIndex => prevIndex + 1);
+          }
+        }
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="aling-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Seja bem-vindo ao meu portfólio.</span>
+                        <h1>{`Olá, eu sou Pedro e sou `}<span className="wrap">{text}</span></h1>
+                        <p>Bem-vindo ao meu portfólio! Sou um desenvolvedor de software com experiência em back-end e front-end, trabalhando com linguagens como Java, JavaScript, Python e TypeScript, e frameworks como Angular, Zul, React, Node e React Native. Estou focado em criar soluções inovadoras e elegantes</p>
+                        <button onClick={()=> console.log('connect')}>Vamos nos conectar<ArrowRightCircle size={25} /></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
