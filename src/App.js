db85cf21fbd25afe23fafb6aee07894179f1668e
Pedro-Wilker <pedrowilker1516@gmail.com>
2023-11-03 12:09:45 -0300

    adicionando imagens dos projetos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {Banner} from './components/Banner.js';
 import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <NavBar/>
       <Banner/>
       <Skills />
+      <Projects />
     </div>
   );
 }
